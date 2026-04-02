5f84a373b5f081f5e8df92f3832286b606f550d9
egnorris <evangnorris@gmail.com>
2026-04-02 18:21:10 +0000

    Added default values for mutationa rate and crossover points

--- Support.py
+++ Support.py
@@ -1,13 +1,15 @@
 import numpy as np
 
-defaultKwargs = {"rMin": 20, "rMax": 75, "d": (180,180), "s": 5, "p": 12}
+defaultKwargs = {"rMin": 20, "rMax": 75, "d": (180,180), "s": 5, "p": 12, "mR":0.1, "cP":2}
 
 keywords = {
     "rMin":["rMin", "minR", "r0", "minimumR", "rMinimum", "minimumRadius"],
     "rMax":["rMax", "maxR", "r1", "maximumR", "rMaximum", "maximumRadius"],
     "d":["domain", "d", "dom"],
     "s":["smoothness", "s", "sigma"],
-    "p":["precision", "p", "binaryPrecision"]
+    "p":["precision", "p", "binaryPrecision"],
+    "mR": ["mR", "mutationRate", "mutR", "mutRate"],
+    "cP": ["cP", "crossoverPoints", "crossPoints"],
     }
 
 def getkwarg(kwargs, default, keywords):

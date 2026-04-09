5e6c74a556270b480f274fe7cc157566393b39f4
egnorris <evangnorris@gmail.com>
2026-04-09 17:08:09 -0400

    rework testing functions to support reworked generation structurea and to utilize new logging and plotting functionality

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ outputDirectory = "data"
 try:
     os.mkdir(f"{outputDirectory}")
 except FileExistsError:
-    print(f"{outputDirectory} Exists")
+    print(f"Directory: {outputDirectory} Exists")
 
 def writePopulationData(pop, fname):
     df = pd.DataFrame({
@@ -30,10 +30,8 @@ def writePopulationData(pop, fname):
     df.to_csv(f"{fname}.csv")
     d = {
         "Fitness": pop.fitness,
-        "Profile": pop.images,
         "Chromosome": pop.chromosomes,
         "Scattered Power": pop.scatteredPower,
-        "Multipole Coefficients": pop.multipoles,
         "Integral": pop.integral,
         "Mean Absolute Error": pop.meanAbsoluteError,
         "Mean Relative Error": pop.meanRelativeError,
@@ -54,14 +52,14 @@ def plotPopulationFitness(pop, fname):
 def plotPopulationProfiles(pop, fname):
     n = pop.nProfiles
     nroot = int(np.round(np.sqrt(pop.nProfiles)))
-    plt.figure(figsize=(15,15))
+    plt.figure(figsize=(2*nroot,2*nroot))
     for i in range(n):
         plt.subplot(nroot, nroot,i+1)
         plt.xticks([])
         plt.yticks([])
         plt.grid(False)
         plt.imshow(pop.images[i, :, :], cmap=plt.cm.binary)
-        plt.xlabel(f"int: {int(np.round(pop.integral[i]))} - rmse: {np.round(pop.rootMeanSquaredError[i], 2)}")
+        plt.xlabel(f"fit: {np.round(pop.fitness[i], 2)} - rmse: {np.round(pop.rootMeanSquaredError[i], 2)}")
     plt.show()
     plt.savefig(f"{fname}Profiles.png")
     plt.close()
@@ -71,7 +69,7 @@ def plotPopulationScattering(pop, fname):
     x = np.linspace(0, 1, 100)
     n = pop.nProfiles
     nroot = int(np.round(np.sqrt(pop.nProfiles)))
-    plt.figure(figsize=(15,15))
+    plt.figure(figsize=(2*nroot,2*nroot))
     for i in range(n):
         plt.subplot(nroot, nroot,i+1)
         plt.xticks([])
@@ -80,7 +78,7 @@ def plotPopulationScattering(pop, fname):
         plt.plot(pop.wavelengths, pop.objective, zorder = -1, color='black')
         plt.plot(pop.wavelengths, pop.scatteredPower[i, :], zorder = -1)
         plt.fill_between(pop.wavelengths, pop.objective, pop.scatteredPower[i, :], color='red', alpha = 0.2)
-        plt.xlabel(f"int: {int(np.round(pop.integral[i]))} - rmse: {np.round(pop.rootMeanSquaredError[i], 2)}")
+        plt.xlabel(f"fit: {np.round(pop.fitness[i], 2)} - rmse: {np.round(pop.rootMeanSquaredError[i], 2)}")
     plt.xlim(300, 800)
     #plt.xlabel("Wavelength (nm)")
     #plt.ylabel("Scattered Power")
@@ -89,74 +87,61 @@ def plotPopulationScattering(pop, fname):
 
 
 
-nGenerations = 20
-nProfiles = 49
+nGenerations = 300
 
-mseEvolution = []
-rmseEvolution = []
-fitnessEvolution = []
-intEvolution = []
-N = [0]
-i = 0
-pop = invdes.Population(nProfiles, 30,"/media/work/evan/deep_learning_data/trained_models", mR = 0.1, cP = 1)
+generationalFitness = []
+generationalIntegral = []
 
-plt.imshow(pop.objectivePoly)
-plt.savefig(f"{outputDirectory}/obj.png")
+pop = invdes.Population(
+    nVertices=30,
+    modelDirectory="/media/work/evan/deep_learning_data/trained_models",
+    rMin=10, rMax=75, d=(180,180), s=5, p=12,
+    mR = 0.10, cP = 1,
+    l=[1,2,2], m=[1,1,2], f=['E', 'H'])
+
+
+pop.defineObjective(profileType = 'fou', termsF=5)
+pop.initialize(nT=30, nR=30, nC=5, nF=5, nP=30)
+pop.displayParameters(outDir="data")
+pop.writeLoss(iGen=0, outDir="data") 
+pop.plotSelectPerformers(outputDirectory, "0")
+
+plt.imshow(pop.objImage, cmap=plt.cm.binary)
+plt.savefig(f"{outputDirectory}/objProfile.png")
 plt.close()
 
-
-plotPopulationFitness(pop, f"{outputDirectory}/gen0")
-plotPopulationProfiles(pop, f"{outputDirectory}/gen0")
-plotPopulationScattering(pop, f"{outputDirectory}/gen0")
-writePopulationData(pop, f"{outputDirectory}/gen0")
+generationalFitness.append(pop.fitness)
+generationalIntegral.append(pop.integral)
 
 
-mseEvolution.append(np.mean(pop.meanSquaredError))
-rmseEvolution.append(np.mean(pop.rootMeanSquaredError))
-fitnessEvolution.append(np.mean(pop.fitness))
-intEvolution.append(np.mean(pop.integral))
 
 for n in range(nGenerations):
     x = (0.5-0.25)*np.random.rand() + 0.25
-    print(f"Generation: {n+1}")
     pop.newGeneration(x)
-    print(f"Maximum Fitness: {np.max(pop.fitness)}")
-    print(f"Minium Fitness: {np.min(pop.fitness)}")
-    print(f"Mean Fitness: {np.mean(pop.fitness)}")
-    print(f"Median Fitness: {np.median(pop.fitness)}")
-    plotPopulationFitness(pop, f"{outputDirectory}/gen{n+1}")
-    plotPopulationProfiles(pop, f"{outputDirectory}/gen{n+1}")
-    plotPopulationScattering(pop, f"{outputDirectory}/gen{n+1}")
-    writePopulationData(pop, f"{outputDirectory}/gen{n+1}")
-    N.append(n+1)
-    mseEvolution.append(np.mean(pop.meanSquaredError))
-    rmseEvolution.append(np.mean(pop.rootMeanSquaredError))
-    fitnessEvolution.append(np.mean(pop.fitness))
-    intEvolution.append(np.mean(pop.integral))
+    pop.writeLoss(iGen=n+1, growthRate=x, outDir="data")
+    pop.plotSelectPerformers(outputDirectory, f"{n+1}")
+    generationalFitness.append(pop.fitness)
+    generationalIntegral.append(pop.integral)
+
+
+xmax = np.max(generationalIntegral)
+xmin = np.min(generationalIntegral)
 
 
 
-plt.scatter(N,fitnessEvolution)
-plt.xlabel("Generations")
-plt.ylabel("Average Fitness")
-plt.savefig(f"{outputDirectory}/FitnessEvolution.png")
-plt.close()
+ymax = np.max(generationalFitness)
+ymin = np.min(generationalFitness)
 
-plt.scatter(N,intEvolution)
-plt.xlabel("Generations")
-plt.ylabel("Average Integral")
-plt.savefig(f"{outputDirectory}/IntegralEvolution.png")
-plt.close()
+xmax = min(xmax, 200)
+ymax = min(ymax, 1)
 
-plt.scatter(N,mseEvolution)
-plt.xlabel("Generations")
-plt.ylabel("Average Root Mean Squared Error")
-plt.savefig(f"{outputDirectory}/RmseEvolution.png")
-plt.close()
-
-plt.scatter(N,rmseEvolution)
-plt.xlabel("Generations")
-plt.ylabel("Average Mean Squared Error")
-plt.savefig(f"{outputDirectory}/MseEvolution.png")
-plt.close()
-
+for i in range(len(generationalFitness)):
+    plt.plot(np.linspace(xmin, xmax, 10), invdes.scale(np.linspace(xmin, xmax, 10)), zorder = -1, lw=1, c='black')
+    plt.scatter(generationalIntegral[i], generationalFitness[i])
+    plt.xlim((xmin, xmax))
+    plt.ylim((ymin, ymax))
+    plt.title(f"Generation {i} Fitness")
+    plt.xlabel("Object - Proposal Gap Area")
+    plt.ylabel("Fitness")
+    plt.savefig(f"{outputDirectory}/fitnessGen{i}.png")
+    plt.close()

4b0b1235e8b5999a83fa9f8d380cea01d803ce00
egnorris <evangnorris@gmail.com>
2026-04-14 14:55:17 -0400

    bash script for the two testing cases, makes movies at the end

--- testing.sh
+++ testing.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-python3 test.py 'rmse'
-python3 test.py 'mse'
-python3 test.py 'mae'
-python3 test.py 'mre'
-python3 test.py 'gap'
+for ft in {"rmse",'mse','mae','mre','gap'}; do
 
-./makeMovie.sh
+    directory="$ft-TestGauss"
+    python3 run.py -out $directory -tp 'objScatteredPower2.txt' -ng 150 -ft $ft -seed 0
+
+    ffmpeg -y -f image2 -framerate 15 -i $directory/gen%dTopPerformers.png -loop -1 TopPerformerEvolution-$ft-gauss.mp4
+    ffmpeg -y -f image2 -framerate 15 -i $directory/gen%dBottomPerformers.png -loop -1 BottomPerformerEvolution-$ft-gauss.mp4
+    rm -rf $directory/gen*
+
+    directory="$ft-TestMultiPeak"
+    python3 run.py -out $directory -tp 'objScatteredPower1.txt' -ng 150 -ft $ft -seed 0
+
+    ffmpeg -y -f image2 -framerate 15 -i $directory/gen%dTopPerformers.png -loop -1 TopPerformerEvolution-$ft-multipeak.mp4
+    ffmpeg -y -f image2 -framerate 15 -i $directory/gen%dBottomPerformers.png -loop -1 BottomPerformerEvolution-$ft-multipeak.mp4
+    rm -rf $directory/gen*
+
+done
+
